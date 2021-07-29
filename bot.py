34e2da10571cdb5b9bd6405405600d99796c3d48
philly <phillychi3@gmail.com>
2021-07-30 00:05:18 +0800

    fix?

--- bot.py
+++ bot.py
@@ -8,16 +8,14 @@ from time import sleep
 
 bot = commands.Bot(command_prefix= 'r')
 #bot.remove_command("help")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless') 
 
 
-chrome_options=Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument("--disable-notifications")
-browser = webdriver.Chrome('./chromedriver.exe', chrome_options=chrome_options)
 rolllinklist=["https://youtu.be/dQw4w9WgXcQ","https://www.youtube.com/watch?v=dQw4w9WgXcQ"]
 
 with open('set.json', 'r', encoding='utf8') as jfile:
-	jdata = json.load(jfile)
+    jdata = json.load(jfile)
 
 @bot.event
 async def on_ready():
@@ -33,20 +31,25 @@ async def on_message(msg):
     rolltest=msg.content
     rolllink=msg.content[0:4]
     rollid=msg.id
-    if rolllink=="http": 
-        if rolltest in rolllinklist:
+    if rolllink=="http":
+        chrome = webdriver.Chrome(chrome_options=chrome_options)
+        chrome.get(rolltest) 
+        print(chrome.current_url)
+        if chrome.current_url in rolllinklist:
             await msg.channel.send("警告 辨識結果為rick roll")
             de = await msg.channel.fetch_message(rollid)
             await de.delete()
         else:
-            browser.get(rolltest)
+            chrome.get(rolltest)
             await asyncio.sleep(5)
             #sleep(5)
-            webroll=browser.current_url        
-            browser.close()
-            browser.quit()
+            webroll=chrome.current_url        
+            chrome.close()
+            chrome.quit()
             if webroll in rolllinklist:
                 await msg.channel.send("警告 辨識結果為rick roll")
                 de = await msg.channel.fetch_message(rollid)
                 await de.delete()
+        chrome.close()
+
 bot.run(jdata['TOKEN'])
