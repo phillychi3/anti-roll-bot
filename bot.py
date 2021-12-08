397ca394c8dccf509a22075320f751c052543729
philly <phillychi3@gmail.com>
2021-12-09 01:02:33 +0800

    fix some bug

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import json
 import asyncio
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
 bot = commands.Bot(command_prefix= 'r')
@@ -12,7 +13,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless') 
 
 
-rolllinklist=["https://youtu.be/dQw4w9WgXcQ","https://www.youtube.com/watch?v=dQw4w9WgXcQ"]
+rolllinklist=["https://youtu.be/dQw4w9WgXcQ","https://www.youtube.com/watch?v=dQw4w9WgXcQ","https://www.youtube.com/watch?v=iik25wqIuFo","https://youtu.be/iik25wqIuFo","https://www.youtube.com/watch?v=DLzxrzFCyOs","https://youtu.be/DLzxrzFCyOs"]
 
 with open('set.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
@@ -32,10 +33,9 @@ async def on_message(msg):
     rolllink=msg.content[0:4]
     rollid=msg.id
     if rolllink=="http":
-        chrome = webdriver.Chrome(chrome_options=chrome_options)
-        chrome.get(rolltest) 
-        print(chrome.current_url)
-        if chrome.current_url in rolllinklist:
+        chrome = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
+        #chrome = webdriver.Chrome(chrome_options=chrome_options)
+        if rolltest in rolllinklist:
             await msg.channel.send("警告 辨識結果為rick roll")
             de = await msg.channel.fetch_message(rollid)
             await de.delete()
--- testweb.py
+++ testweb.py
@@ -1,6 +1,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 #req_url = "https://reurl.cc/gWbmQb"
 req_url = "https://discordgift.site/store/en-US/p/discord--discord-nitro"
@@ -9,13 +10,13 @@ req_url = "https://discordgift.site/store/en-US/p/discord--discord-nitro"
 chrome_options=Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument("--disable-notifications")
-browser = webdriver.Chrome('./chromedriver.exe', chrome_options=chrome_options)
-
-browser.get(req_url)
+#browser = webdriver.Chrome('./chromedriver.exe', chrome_options=chrome_options)
+chrome = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
+chrome.get(req_url)
 sleep(5)
 
-print(browser.current_url)
+print(chrome.current_url)
 
-browser.close()
+chrome.close()
 
-browser.quit()
+chrome.quit()
